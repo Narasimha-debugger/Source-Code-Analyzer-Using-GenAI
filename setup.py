363232cc43d3391053ceec839dbf0c89aecaf970
Narasimha-debugger <nagnarasimhareddy84@gmail.com>
2024-04-15 21:20:02 +0530

    Setup Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Source COde Analyzer',
+    version='0.0.0',
+    author='Naganarasimha',
+    author_email='naganarasimhareddy84@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
